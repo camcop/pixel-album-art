524c3f1fcd1ba65e2062742df30e11a3b83eadf0
Cameron Copland <cjcopland@gmail.com>
2024-09-10 12:39:53 +0100

    Refactor to simplify

--- main.py
+++ main.py
@@ -1,78 +1,78 @@
 import os
-import logging
-import time
+import shutil
 import requests
-
 from dotenv import load_dotenv
-from unidecode import unidecode
-
-from config import BOARD_DRIVE_LETTER, IMAGE_DIR, INPUT_IMAGE, OUTPUT_IMAGE, LASTFM_PUBLIC_API_KEY, LOOP_SLEEP_S
-from convert import convert, resize
-from write_to_board import copy_file, create_or_update_log
-from download import download_cover, fetch_last_played
-
-load_dotenv()
-
-LASTFM_USER = os.getenv('LASTFM_USER')
-output_filename = ''
-
+import time
+import logging
+from PIL import Image
+import io
+from config import (
+    OUTPUT_IMAGE,
+    BOARD_DRIVE_LETTER,
+    LASTFM_PUBLIC_API_KEY,
+    LOOP_SLEEP_S,
+    REQUEST_TIMEOUT_S,
+    MATRIX_WIDTH,
+    MATRIX_HEIGHT,
+)
+    
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-def main():
-    initial_iteration = True
-    lastplayed_album = ''
-    lastplayed_artist = ''
-    current_art = ''
-    lastfm_session = requests.Session()
-    image_session = requests.Session()
+load_dotenv()
+LASTFM_USER = os.getenv('LASTFM_USER')
+LASTFM_URL = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks'
+CONVERT_MODE = 'RGB'
 
+def main():
+
+    image_url = ''
     while True:
-        if not initial_iteration:
-            logger.debug(f'Sleeping for {LOOP_SLEEP_S} seconds')
-            time.sleep(LOOP_SLEEP_S)
-        initial_iteration = False
+
+        logger.debug('sleeping')
+        time.sleep(LOOP_SLEEP_S)
 
         try:
-            lastplayed_trackname, lastplayed_artist, lastplayed_album, lastplayed_image_url = fetch_last_played(lastfm_session, LASTFM_PUBLIC_API_KEY, LASTFM_USER)
-            logger.info(f'Last played: "{lastplayed_trackname}" by "{lastplayed_artist}" from "{lastplayed_album}"')
-            logger.info(f'Last played image URL: {lastplayed_image_url}')
-
+            logger.debug('requesting')
+            response = requests.get(f'{LASTFM_URL}&user={LASTFM_USER}&api_key={LASTFM_PUBLIC_API_KEY}&format=json', timeout=REQUEST_TIMEOUT_S)
         except Exception as e:
-            logger.error(f'Error fetching last played track: {e}')
+            logger.error(e)
             continue
 
-        if lastplayed_image_url is None:
-            logger.warning('No album or artist found; skipping to the next iteration')
+        if response.status_code != 200:
             continue
         
-        # Remove unsafe characters from album and artist names
-        remove_chars = str.maketrans('', '', r' /\\:*?"<>|')
-        lastplayed_album_cleaned = unidecode(lastplayed_album).translate(remove_chars)
-        lastplayed_artist_cleaned = unidecode(lastplayed_artist).translate(remove_chars)
-        output_filename = f'{lastplayed_album_cleaned}-by-{lastplayed_artist_cleaned}.jpeg'
+        try:
+            obj = response.json()
+            image_url_api = obj.get('recenttracks', {}).get('track', [])[0].get('image', [None, None, None, None])[3]['#text']
+            logger.debug(f'{image_url_api=}')
+        except Exception as e:
+            logger.error(e)
+            continue
 
-        if lastplayed_album_cleaned != current_art:
-            logger.info(f'New album detected: {lastplayed_album_cleaned}. Downloading album art...')
-            try:
-                downloaded_file = download_cover(image_session, lastplayed_image_url, output_filename)
-                logger.info(f'Successfully downloaded cover art: {downloaded_file}')
-            except Exception as e:
-                logger.error(f'Failed to download album art: {e}')
-                continue
-            current_art = lastplayed_album_cleaned
-
-            try:
-                resized_file = resize(output_filename, 64, 64)
-                converted_file = convert(resized_file, 'bmp')
-                copy_file(os.path.join(IMAGE_DIR, converted_file), OUTPUT_IMAGE)
-                create_or_update_log(output_filename)
-                logger.debug(f'Processed and updated board with new image: {converted_file}')
-            except Exception as e:
-                logger.error(f'Error in processing images: {e}')
+        if image_url_api == image_url:
+            logger.debug('image hasnt changed')
+            continue
         else:
-            logger.debug('No new album detected; continuing loop')
+            image_url = image_url_api
 
+        try:
+            response = requests.get(image_url, timeout=REQUEST_TIMEOUT_S)
+        except Exception as e:
+            logger.error(e)
+            continue
 
-if __name__ == '__main__':
-    main()
+        if response.status_code != 200:
+            logger.error(e)
+            continue
+        
+        try:
+            image = Image.open(io.BytesIO(response.content))
+            image = image.resize((MATRIX_WIDTH, MATRIX_HEIGHT)).convert(CONVERT_MODE)
+            image.save(os.path.join(BOARD_DRIVE_LETTER + ':', OUTPUT_IMAGE), OUTPUT_IMAGE.split('.')[-1])
+        except Exception as e:
+            logger.error(e)
+            logger.debug(response.content)
+            continue
+
+main()
